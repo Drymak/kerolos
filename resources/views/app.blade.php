ca8ccdf099b7e3ce5af26d5e40aadc26079a8e48
Kerolos Gad <kokokamal390@gmail.com>
2022-09-06 14:11:41 +0200

    Fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,7 +19,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Resume Webpages</title>
 </head>
 <body class="">
