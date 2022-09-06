30d1406b4c76211dcbd74d4d3becf5e18bf19ee5
Kerolos Gad <kokokamal390@gmail.com>
2022-09-06 13:43:21 +0200

    Deploy

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kerolos Gad</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+    <h1 class="text-5xl font-bold text-center m-10">
+      Kerolos Gad
+    </h1>
+  </body>
+</html>
